ed32b531ecccece624cab3268837e7c8b9d6cbef
Snild Dolkow <snild@dolkow.se>
2018-03-18 21:44:56 +0100

    xnbread, initial commit
    No, not xn-bread -- xnb-read. :)
    
    This first commit can successfully spit out an uncompressed version
    of Stardew Valley's /Content/Characters/schedules/Clint.xnb
    
    I'll probably run into more trouble as I start trying some of the
    bigger files. Not to mention that there's nothing to handle the
    actual XNB content payload yet.
    
    But let's see how far we get before I find something else to do. :)

---
+++ lzx.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+#coding=utf8
+
+# based on format description at https://msdn.microsoft.com/en-us/library/bb417343.aspx#lzxintro
+# with some quirks help from https://github.com/kyz/libmspack/blob/master/libmspack/mspack/lzxd.c
+
+import struct
+
+from util import TODO, clamp
+
+# block types:
+VERBATIM     = 1
+ALIGNED      = 2
+UNCOMPRESSED = 3
+
+# misc
+WINDOW_BITS = 16 # XXX valid for .xnb lzx data; may vary in others!
+WINDOW_SIZE = 1 << WINDOW_BITS
+NUM_POSITION_SLOTS = WINDOW_BITS * 2 # this will be wrong for winbits 20 and 21
+MIN_MATCH = 2
+MAX_MATCH = 257
+NUM_CHARS = 256
+NUM_PRIMARY_LENGTHS = 7
+EXTRA_BITS = [clamp(i // 2 - 1, 0, 17) for i in range(50)]
+BASE_POSITION = [sum(1 << x for x in EXTRA_BITS[:i]) for i in range(50)]
+
+# pre-tree
+PRETREE_SIZE = 20
+PRETREE_TABLE_BITS = 6
+# main tree
+MAIN_SIZE = NUM_CHARS + NUM_POSITION_SLOTS * 8
+MAIN_TABLE_BITS = 12
+# secondary tree (a.k.a. "length tree"; a difficult name to use); beta for short
+BETA_SIZE = 249
+BETA_TABLE_BITS = 12
+
+
+class BitStream(object):
+	''' the LZX format bit stream puts bits in 16-bit entities, with
+	    little-endian byte order '''
+	def __init__(self, bytes):
+		self.bytes = bytes
+		self.nextbyte = 0
+		self.cache = 0 # essentially a queue of unused bits
+		self.avail = 0 # bits in cache
+
+	def peek(self, bits):
+		while self.avail < bits and self.nextbyte < len(self.bytes):
+			self.avail += 16
+			self.cache <<= 16
+			self.cache |= self.bytes[self.nextbyte]
+			self.cache |= self.bytes[self.nextbyte+1] << 8
+			self.nextbyte += 2
+		if self.avail >= bits:
+			return self.cache >> (self.avail - bits)
+		else:
+			return self.cache << (bits - self.avail) # zero-pad
+
+	def take(self, bits):
+		if bits > self.avail:
+			raise Exception('taking %d bits from only %d' % (bits, self.avail))
+		self.avail -= bits
+		self.cache &= (1 << self.avail) - 1
+
+	def read(self, bits):
+		val = self.peek(bits)
+		self.take(bits)
+		return val
+
+class LookupTable(object):
+	def __init__(self, nbits, symbol_lengths):
+		self.nbits = nbits
+		self.table = table = (1 << nbits) * [None]
+		self.symbol_lengths = symbol_lengths
+
+		assert all(length <= nbits for length in symbol_lengths)
+		assert all(length >= 0 for length in symbol_lengths)
+
+		next = (1 << nbits) - 1
+		nsyms = len(symbol_lengths)
+		for bits in range(nbits, 0, -1):
+			for val in range(nsyms-1, -1, -1):
+				if symbol_lengths[val] == bits:
+					nslots = 1 << (nbits - bits)
+					for x in range(nslots):
+						table[next-x] = val
+					next -= nslots
+		if False:
+			for peeked, val in enumerate(table):
+				print('LUT {:06b}'.format(peeked), val, symbol_lengths[val])
+		assert next == -1
+		assert not any(x is None for x in table), 'empty lookup table slot'
+		populated = set(i for i, v in enumerate(symbol_lengths) if v > 0)
+		assert populated == set(self.table)
+
+	def read_from(self, bitstream):
+		peeked = bitstream.peek(self.nbits)
+		val = self.table[peeked]
+		bitstream.take(self.symbol_lengths[val])
+		return val
+
+def decompress_block(cbuf, dbuf):
+	first_block = True # TODO: take as param
+	main_lengths = memoryview(bytearray(MAIN_SIZE)) # TODO: take as param / store state?
+	beta_lengths = memoryview(bytearray(BETA_SIZE)) # TODO: take as param / store state?
+	cbuf = BitStream(cbuf)
+	dpos = 0
+	lru = [1, 1, 1]
+
+	if first_block:
+		transformed = cbuf.read(1)
+		if transformed:
+			TODO("intel call instruction transform not supported")
+	while dpos < len(dbuf):
+		# TODO: block remaining bytes?
+		blocktype = cbuf.read(3)
+		blocksize = cbuf.read(24) # uncompressed size of this block
+		if blocktype == VERBATIM:
+			update_lengths(cbuf, main_lengths[:NUM_CHARS])
+			update_lengths(cbuf, main_lengths[NUM_CHARS:])
+			maintree = LookupTable(MAIN_TABLE_BITS, main_lengths)
+			update_lengths(cbuf, beta_lengths)
+			betatree = LookupTable(BETA_TABLE_BITS, beta_lengths)
+		else:
+			TODO("no implementation for block type %d" % blocktype)
+		while blocksize > 0:
+			val = maintree.read_from(cbuf)
+			if val < NUM_CHARS:
+				dbuf[dpos] = val
+				dpos += 1
+				blocksize -= 1
+				continue
+			match_length = (val - NUM_CHARS) & NUM_PRIMARY_LENGTHS
+			if match_length == NUM_PRIMARY_LENGTHS:
+				match_length += MIN_MATCH + betatree.read_from(cbuf)
+			else:
+				match_length += MIN_MATCH
+			position_slot = (val - NUM_CHARS) >> 3
+			if position_slot <= 2:
+				match_offset = lru[position_slot]
+				lru[position_slot] = lru[0]
+				lru[0] = match_offset
+			elif position_slot == 3:
+				match_offset = 1
+			else:
+				extra = EXTRA_BITS[position_slot]
+				if blocktype == VERBATIM:
+					if extra > 0:
+						verbatim_bits = cbuf.read(extra)
+					formatted_offset = BASE_POSITION[position_slot] + verbatim_bits
+				else:
+					TODO()
+				match_offset = formatted_offset - 2
+				lru[2] = lru[1]
+				lru[1] = lru[0]
+				lru[0] = match_offset
+			# now, copy!
+			for i in range(match_length):
+				dbuf[dpos + i] = dbuf[dpos + i - match_offset]
+			dpos += match_length
+			blocksize -= match_length
+
+def update_lengths(bitstream, lengths):
+	pretree = LookupTable(PRETREE_TABLE_BITS,
+	                      [bitstream.read(4) for i in range(PRETREE_SIZE)])
+	repeats_left = 0
+	for x in range(len(lengths)):
+		if repeats_left > 0:
+			lengths[x] = lengths[x-1]
+			repeats_left -= 1
+			continue
+		code = pretree.read_from(bitstream)
+		if code == 17:
+			repeats_left = 3 + bitstream.read(4)
+			lengths[x] = 0
+		elif code == 18:
+			repeats_left = 19 + bitstream.read(5)
+			lengths[x] = 0
+		else:
+			if code == 19:
+				repeats_left = 3 + bitstream.read(1)
+				code = pretree.read_from(bitstream)
+			lengths[x] = (lengths[x] - code) % 17
+	if False:
+		for i,v in enumerate(lengths):
+			if v != 0:
+				print('update_lengths %3d => %2d' % (i, v))
+	return lengths
+
+def decompress(cdata, inputsize, outputsize):
+	cdata = memoryview(cdata)
+	ddata = memoryview(bytearray(outputsize))
+	cpos = 0
+	dpos = 0
+	assert len(cdata) == inputsize
+	while dpos < outputsize and cpos < inputsize:
+		if cdata[cpos] == 0xff:
+			fsize, bsize = struct.unpack('>HH', cdata[cpos+1:cpos+5])
+			cpos += 5
+		else:
+			TODO("haven't tested with blocks that don't start with 0xff")
+		assert fsize > 0
+		assert bsize > 0
+
+		decompress_block(cdata[cpos:cpos+bsize], ddata[dpos:dpos+fsize])
+
+		cpos += bsize
+		dpos += fsize
+	assert dpos == outputsize
+	return ddata
---
+++ util.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#coding=utf8
+
+import struct
+
+from collections import namedtuple
+
+def TODO(msg = "Not implemented yet."):
+	assert 0, msg
+
+def _dumphexline(line):
+	fmt = ' '.join(['%02x'] * len(line))
+	args = tuple(int(c) for c in line)
+	print(fmt % args)
+
+def dumphex(s):
+	pos = 0
+	end = len(s)
+	while pos < end:
+		_dumphexline(s[pos:pos+16])
+		pos += 16
+
+def define_tuple(name, fmt, expected_size, fields):
+	tup = namedtuple(name, fields)
+	tup.format = fmt
+	tup.size = expected_size
+	print(tup._make)
+	assert struct.calcsize(fmt) == expected_size
+	return tup
+
+def clamp(v, lo, hi):
+	return max(lo, min(hi, v))
