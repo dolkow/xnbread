c74a615cb1291ff581e2416bd239012d5a7e638c
Snild Dolkow <snild@dolkow.se>
2018-03-19 06:27:02 +0100

    Move file reading to a separate function

--- xnbread.py
+++ xnbread.py
@@ -11,22 +11,25 @@ FLAG_BIT_COMPRESSED = 0x80
 Header = define_tuple('Header', '<3scBBII', 14,
 		'magic target version flags fsize dsize')
 
+def read(f):
+	header = Header._make(struct.unpack(Header.format, f.read(Header.size)))
+	data = f.read(header.fsize - Header.size)
+	dumphex(data)
+	assert header.magic == b'XNB'
+	assert header.target in b'wmx'
+	assert header.version == 5
+	assert header.fsize == len(data) + Header.size
+	assert header.flags & KNOWN_FLAGS == header.flags
+	if header.flags & FLAG_BIT_COMPRESSED:
+		import lzx
+		csize = header.fsize - Header.size
+		data = lzx.decompress(data, csize, header.dsize)
+	else:
+		TODO("uncompressed data not implemented yet")
+	dumphex(data)
+
 if __name__ == '__main__':
 	import sys
 	for filename in sys.argv[1:]:
 		with open(filename, 'rb') as f:
-			header = Header._make(struct.unpack(Header.format, f.read(Header.size)))
-			data = f.read()
-			dumphex(data)
-			assert header.magic == b'XNB'
-			assert header.target in b'wmx'
-			assert header.version == 5
-			assert header.fsize == len(data) + Header.size
-			assert header.flags & KNOWN_FLAGS == header.flags
-			if header.flags & FLAG_BIT_COMPRESSED:
-				import lzx
-				csize = header.fsize - Header.size
-				data = lzx.decompress(data, csize, header.dsize)
-			else:
-				TODO("uncompressed data not implemented yet")
-			dumphex(data)
+			read(f)
