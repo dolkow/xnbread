9896f5e1c71e5a38c5102a4b808fad3f42969245
Snild Dolkow <snild@dolkow.se>
2018-03-22 20:37:41 +0100

    Add linkxnb.sh
    This script will create symlinks to all .xnb files in a dir tree.
    Useful for setting up tests for all files from a game.

---
+++ linkxnb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -e "$(pwd)/$(basename $0)" ]; then
+	echo "you probably want to go into another directory to do this."
+	exit 1
+fi
+
+ROOT="$(readlink -e """$1""")"
+
+find "$ROOT" -type d | while read SRC; do
+	DST="${SRC#$ROOT/}"
+	if readlink -e "$SRC"/*.xnb > /dev/null; then
+		mkdir -p "./$DST"
+		ln -s "$SRC"/*.xnb "./$DST" > /dev/null
+	fi
+done
