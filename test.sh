fdb60ae267a506ddb06083d8d0c16594ca92ab27
Snild Dolkow <snild@dolkow.se>
2018-03-23 06:22:25 +0100

    Print number of passes/failures in test.sh

--- test.sh
+++ test.sh
@@ -8,6 +8,11 @@ RED="$(tput setaf 1)"
 
 SCRATCH=$(mktemp)
 
+PASSES=0
+FAILS=0
+
+declare -a FAILNAMES
+
 function testone() {
 	xnb="$1"
 	out="${xnb%.xnb}.out"
@@ -15,8 +20,11 @@ function testone() {
 	./xnbread.py -r "$xnb" > "$SCRATCH"
 	if diff -q "$out" "$SCRATCH" > /dev/null; then
 		echo "${GREEN}OK!${RESET}"
+		PASSES=$((PASSES+1))
 	else
 		echo "${RED}Failure.${RESET}"
+		FAILS=$((FAILS+1))
+		FAILNAMES+=("$xnb")
 	fi
 }
 
@@ -26,9 +34,20 @@ if [ $# -gt 0 ]; then
 		shift
 	done
 else
-	find tests -name '*.xnb' | while read xnb; do
+	readarray -t xnbfiles < <(find tests -name '*.xnb')
+	for xnb in "${xnbfiles[@]}"; do
 		testone "$xnb"
 	done
 fi
+
+echo
+x=0
+while [ $x -lt $FAILS ]; do
+	echo "Failure ${x}: ${RED}${FAILNAMES[$x]}${RESET}"
+	x=$((x+1))
+done
+
+echo
+echo "Total: ${GREEN}${PASSES} passes, ${RED}${FAILS} failures${RESET}"
 
 rm $SCRATCH
