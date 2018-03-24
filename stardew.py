f0285e1142ff301d238faac10bd0eea0ce89086b
Snild Dolkow <snild@dolkow.se>
2018-03-24 10:59:42 +0100

    Add xTile.Pipeline.TideReader (Stardew Valley-specific)
    Just returns the binary file data.

--- stardew.py
+++ stardew.py
@@ -4,9 +4,14 @@
 # this file contains non-standard types found in Stardew Valley's XNB files.
 
 from readers import *
-from readers.basic import _read_str
+from readers.basic import _read_str, _u32
 
 def _bmxml(factory):
 	# TODO: parse this xml? Nahhh.
 	return _read_str(factory)
 add_reader(_bmxml, 'BmFont.XmlSourceReader')
+
+def _xtile(factory):
+	nbytes = _u32(factory)
+	return factory.stream.read_bytes(nbytes).tobytes()
+add_reader(_xtile, 'xTile.Pipeline.TideReader')
